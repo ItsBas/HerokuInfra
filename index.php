4776d1d77f78e4c48a45db6a6b64aedc6a03dd36
Bas van Dam <dam0026@hz.nl>
2017-03-29 15:57:41 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bas van Dam | Infrastructuur</title>
+</head>
+<body>
+    <h1>Dit is het Heroku project van Bas van Dam.</h1>
+    <?php phpinfo(); ?>
+</body>
+</html>
