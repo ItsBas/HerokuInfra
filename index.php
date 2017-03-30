2a90c430bd774c8ba8a83c2dfebbbe4031a71cfd
Bas van Dam <dam0026@hz.nl>
2017-03-30 11:25:50 +0200

    added php code

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <title>Bas van Dam | Infrastructuur</title>
 </head>
 <body>
-    <h1>Dit is het Heroku project van Bas van Dam.</h1>
+    <h1 style="text-align: center;">Dit is het Heroku project van Bas van Dam.</h1>
+    <h3 style="text-align: center; color: red;">Mijn hele mooie project. De datum van vandaag is: <?= date("D M j G:i:s Y"); ?></h3>
     <?php phpinfo(); ?>
     <p>Dit is gemaakt op 29-3-2017</p>
 </body>
